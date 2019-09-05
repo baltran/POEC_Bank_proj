126467bfa746fdc44a372bbc8a69b40d181d7e86
Victor <daubie_vic@msn.com>
2019-09-05 16:38:14 +0200

    ajout .gitignore log.conf

--- classes/admin.py
+++ classes/admin.py
@@ -18,4 +18,3 @@ class Admin(Utilisateur):
 
 if __name__== "__main__":
     admin = Admin()
-    admin.create_conseiller(('ag1dupont', 'fgfrhfXUkkde$$', 'dupont', 'jean', 'pdupont@gmail.com'), (1, 'ag1dupont', '2000-12-04',  None))
