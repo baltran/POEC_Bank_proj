0555fd49b7524bb2ea254788bca98eaf9212cb03
sadia <ingrachen.sadia@gmail.com>
2019-09-05 15:35:19 +0200

    modif admin, conseiller

--- classes/admin.py
+++ classes/admin.py
@@ -39,6 +39,9 @@ class Admin(Utilisateur):
         print(self.agents)
 
 
+
+
+
 if __name__== "__main__":
     admin = Admin()
     admin.create_conseiller(('ag1dupont', 'fgfrhfXUkkde$$', 'dupont', 'jean', 'pdupont@gmail.com'), (1, 'ag1dupont', '2000-12-04',  None))
