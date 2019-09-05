0c4679f3a63c72a2b5521b00b52c10565a020420
LeandroSPimenta <leandro.s.pimenta@gmail.com>
2019-09-05 15:40:03 +0200

    Résolution merge conseiller

--- classes/conseiller.py
+++ classes/conseiller.py
@@ -5,12 +5,6 @@ from classes.demande import Demande
 class Conseiller(Utilisateur):
     def __init__(self, data_user, data_conseiller):
 
-        super().__init__(user[0], user[1], user[2], user[3],user[4])
-        self.mle = mle
-        self.date_debut = date_debut
-        self.date_fin = date_fin
-
-
         super().__init__(*data_user)
         self.mle = data_conseiller[0]
         self.date_debut = data_conseiller[2]
