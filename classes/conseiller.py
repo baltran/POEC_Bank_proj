048d711d0ef5fa6c42be4ca264531657004e0bce
sadia <ingrachen.sadia@gmail.com>
2019-09-05 14:40:12 +0200

    modif admin, conseiller

--- classes/conseiller.py
+++ classes/conseiller.py
@@ -2,10 +2,9 @@ from classes.utilisateur import Utilisateur
 
 
 class Conseiller(Utilisateur):
-    def __init__(self, mle, date_debut, date_fin, user):
-        super().__init__(user[0], user[1], user[2], user[3],user[4])
-        self.mle = mle
-        self.date_debut = date_debut
-        self.date_fin = date_fin
-
+    def __init__(self, data_user, data_conseiller):
+        super().__init__(*data_user)
+        self.mle = data_conseiller[0]
+        self.date_debut = data_conseiller[2]
+        self.date_fin = data_conseiller[3]
 
