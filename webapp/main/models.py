469e6ceed0371c7661915631eebfbb060624104c
LeandroSPimenta <leandro.s.pimenta@gmail.com>
2019-10-08 11:43:37 +0200

    models: message de conflit effacé

--- webapp/main/models.py
+++ webapp/main/models.py
@@ -6,9 +6,4 @@ from webapp.main.classes.compte import Compte
 from webapp.main.classes.compte_courant import CompteCourant
 from webapp.main.classes.compte_epargne import CompteEpargne
 from webapp.main.classes.operation import Operation
-<<<<<<< HEAD
 from webapp.main.classes.demande import Demande
-=======
-
-
->>>>>>> 3e29b025c223777ea323740b8697627eb3e34b4c
