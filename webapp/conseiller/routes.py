3c118181c9a88d2f0b578263451e8fa053240c7b
LeandroSPimenta <leandro.s.pimenta@gmail.com>
2019-10-09 14:13:11 +0200

    bug fix: doublons du bp conseiller; gestion_demande début boutons

--- webapp/conseiller/routes.py
+++ webapp/conseiller/routes.py
@@ -1,13 +1,17 @@
 from flask import render_template
 from webapp.conseiller import bp
 from webapp.main.classes.demande import Demande
+from webapp.main.requetes import select_all
 # from webapp.main.classes.conseiller import Conseiller
 
 
-@bp.route('/conseiller/gerer_demandes')
+
+@bp.route('/index')
+@bp.route('/gerer_demandes')
 # @login_required
-def index():
-    requests = select_all(Demande)
+def gerer_demandes():
+    # requests = select_all(Demande)
+    requests = Demande.query.all()
     # conseillers = select_all(Conseiller)
     return render_template('conseiller/gerer_demandes.html',
                            title="Gestion des demandes",
