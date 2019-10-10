15b96b6d5926e4961cbd1da493291a4fb2bc2131
ingrachen <ingrachen.sadia@gmail.com>
2019-10-10 11:01:49 +0200

    maj operation

--- webapp/main/classes/operation.py
+++ webapp/main/classes/operation.py
@@ -6,8 +6,10 @@ class Operation(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     done_at = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     valeur = db.Column(db.Integer)
-    compte_src = db.Column(db.Integer, db.ForeignKey('compte.id'))
-    #compte_dest = db.Column(db.Integer, db.ForeignKey('compte.id'))
+    compte_id = db.Column(db.Integer, db.ForeignKey('compte.id'))
+    compte_bis_id = db.Column(db.Integer, db.ForeignKey('compte.id'))
+    operations = db.relationship('Compte', foreign_keys=[compte_id])
+    virements= db.relationship('Compte', foreign_keys=[compte_bis_id])
 
     def afficher(self):
         return {
