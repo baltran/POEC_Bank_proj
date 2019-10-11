61ddc4738acb6466fff62141634d31038e75f488
Victor <daubie_vic@msn.com>
2019-10-12 01:01:49 +0200

    ajout api virement/changement affichage operations

--- webapp/api/comptes.py
+++ webapp/api/comptes.py
@@ -21,11 +21,12 @@ def get_operations(id):
     compte = Compte.query.get_or_404(id)
     if g.current_user.id != compte.titulaire_id:
         return denied_request('Veuillez utiliser un identifiant de compte différent')
-    operations = Operation.to_collection_dict(compte.operations.union_all(compte.virements).order_by(Operation.done_at.desc()),
-                                              page,
-                                              per_page,
-                                              'api.get_operations',
-                                              id=id)
+    operations = Operation.to_collection_dict(
+                    compte.operations.union_all(compte.virements).order_by(Operation.done_at.desc()),
+                    page,
+                    per_page,
+                    'api.get_operations',
+                    id=id)
     operations.update({'solde': compte.solde})
     return jsonify(operations)
 
@@ -75,7 +76,47 @@ def set_withdrawal(id):
     if 'valeur' not in data:
         return bad_request('Veuillez ajouter une valeur')
     try:
-        decrement = int(data['valeur'])
+        decrement = float(data['valeur'])
+    except (ValueError, TypeError):
+        return bad_request('Veuillez mettre un nombre')
+    else:
+        decrement = abs(decrement)
+        solde_tmp = compte.solde - decrement
+        if solde_tmp < 0:
+            if not compte.autorisation_decouvert:
+                return denied_request("Ce compte ne peut pas être à découvert")
+            elif solde_tmp < (0 - (compte.entree_moyenne * compte.taux_decouvert)):
+                return denied_request("L'opération demandée dépasse le seuil de découvert")
+        compte.solde = solde_tmp
+        data_retrait = {'valeur': -decrement, 'compte_id': compte.id, 'type_operation': 'retrait'}
+        operation = Operation(**data_retrait)
+        db.session.add(operation)
+        db.session.commit()
+        return jsonify(operation.to_dict())
+
+
+@bp.route('/comptes/<int:id>/virement', methods=['PUT'])
+@token_auth.login_required
+def set_transfert(id):
+    """ Permet le retrait sur un compte de la personne authentifiée
+        test httpie:
+        http PUT http://localhost:5000/api/comptes/<int:id>/retrait "Authorization:Bearer sCzzknjzsA/VVGstIcvYz7CmJKpFwJ9u"
+    """
+    compte = Compte.query.get_or_404(id)
+    data = request.get_json() or {}
+    if g.current_user.id != compte.titulaire_id:
+        return denied_request('Veuillez utiliser un identifiant de compte différent')
+    if 'valeur' in data and not data['valeur']:
+        return bad_request('Veuillez mettre un nombre')
+    if 'valeur' not in data:
+        return bad_request('Veuillez ajouter une valeur')
+    if ('compte_dest' in data and not data['compte_dest']) or 'compte_dest' not in data:
+        return bad_request('Veuillez mettre un identifiant de compte')
+    compte_dest = Compte.query.get_or_404(data['compte_dest'])
+    if not (compte_dest is not None and compte_dest.id != compte.id):
+        return denied_request('Veuillez utiliser un identifiant de compte bénéficiaire différent')
+    try:
+        decrement = float(data['valeur'])
     except (ValueError, TypeError):
         return bad_request('Veuillez mettre un nombre')
     else:
@@ -86,12 +127,13 @@ def set_withdrawal(id):
                 return conflict_request("Le compte n'a pas d'autorisation de découvert")
             elif solde_tmp < (0 - (compte.entree_moyenne * compte.taux_decouvert)):
                 return conflict_request("L'operation demandée dépasse le seuil de découvert")
-        data = (solde_tmp, compte.rib)
         compte.solde = solde_tmp
-        data_retrait = {'valeur': -decrement, 'compte_id': compte.id, 'type_operation': 'retrait'}
-        operation = Operation(**data_retrait)
+        compte_dest.solde += decrement
+        data_virement = {'valeur': decrement,
+                         'compte_id': compte.id,
+                         'type_operation': 'virement',
+                         'compte_bis_id': compte_dest.id}
+        operation = Operation(**data_virement)
         db.session.add(operation)
         db.session.commit()
         return jsonify(operation.to_dict())
-
-
--- webapp/main/classes/operation.py
+++ webapp/main/classes/operation.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from flask import url_for
 from webapp import db
+from webapp.main.classes.compte import Compte
 from webapp.main.classes.pagination import PaginatedAPIMixin
 import enum
 
@@ -15,7 +16,7 @@ class Operation(PaginatedAPIMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     done_at = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     valeur = db.Column(db.Float)
-    type_operation= db.Column(db.Enum(TypeOp))
+    type_operation = db.Column(db.Enum(TypeOp))
     label = db.Column(db.String)
     compte_id = db.Column(db.Integer, db.ForeignKey('compte.id'))
     compte_bis_id = db.Column(db.Integer, db.ForeignKey('compte.id'))
@@ -24,28 +25,36 @@ class Operation(PaginatedAPIMixin, db.Model):
         return '<Operation {}>'.format(self.id)
 
     def afficher(self):
+        compte = Compte.query.get(self.compte_id)
+        compte_dest = Compte.query.get(self.compte_bis_id or -1)
+        beneficiaire = ""
+        if compte_dest:
+            beneficiaire = (self.compte_bis.titulaire.nom or "") + " " + \
+                       (self.compte_bis.titulaire.prenom or "") + \
+                       " (" + compte_dest.discriminator + ")"
         return {
-            'done_at' : self.done_at,
-            'valeur' : self.valeur,
-            'compte source' : self.compte_id,
-            'compte bis' : self.compte_bis_id,
-            'type operation': self.type_operation.name,
-            'label': self.label
+            'Fait le'     : self.done_at or "",
+            'Valeur'      : self.valeur or "",
+            'Émetteur'    : self.compte_id or "",
+            'Type'        : self.type_operation.name or "",
+            'Bénéficiaire': self.compte_bis_id or "",
+            'Libellé'     : self.label or ""
         }
 
     def to_dict(self):
         data = {
-            'id': self.id,
-            'done_at': self.done_at,
-            'valeur': self.valeur,
-            'compte_id': self.compte_id,
-            'type_operation': self.type_operation.name
+            'id'            : self.id,
+            'done_at'       : self.done_at,
+            'valeur'        : self.valeur,
+            'compte_id'     : self.compte_id,
+            'type_operation': self.type_operation.name,
+            'label'         : self.label
         }
         if self.compte_bis_id:
             data['compte_bis_id'] = self.compte_bis_id
         return data
 
     def from_dict(self, data):
-        for field in ['id', 'done_at', 'valeur', 'compte_id', 'type_operation']:
+        for field in ['id', 'done_at', 'valeur', 'compte_id', 'type_operation', 'label']:
             if field in data:
                 setattr(self, field, data[field])
