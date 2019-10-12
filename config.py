e77d2ccee985d5b50f6272d550f93046de9dedb9
LeandroSPimenta <leandro.s.pimenta@gmail.com>
2019-10-13 01:56:25 +0200

    e-mail nouveau compte: problème de connexion avec le seveur

--- config.py
+++ config.py
@@ -12,13 +12,10 @@ class Config(object):
     SQLALCHEMY_ENGINE_OPTION = {
         'poolclass': SingletonThreadPool
     }
-
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = os.environ.get('MAIL_PORT')
-    # MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
-    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_USERNAME = 'poec.python.2019@gmail.com'
-    MAIL_PASSWORD = 'MotDePassePOEC'
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     LANGUAGES = ['fr', 'en']
     ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg'])
     # UPLOAD_FOLDER = '/Desktop'
